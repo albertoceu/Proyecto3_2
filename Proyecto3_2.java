f0f1df12ebe682b7121ae711901fa772d4a86d66
AlbertoCeu <abernardez@ceu.es>
2023-11-22 09:02:43 +0100

    Commit1

---
+++ Proyecto3_2.java
@@ -0,0 +1,10 @@
+package CEUTema3;
+
+public class Clase3_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Bienvenido a Git Tema 3 Practica 2");
+	}
+
+}
