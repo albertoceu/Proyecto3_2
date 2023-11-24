1ee9b8b628fddc19c3873e0c30bfd781af412588
AlbertoCeu <abernardez@ceu.es>
2023-11-24 06:36:07 +0100

    Añadido Nombre

--- Clase3_2.java
+++ Clase3_2.java
@@ -5,6 +5,10 @@ public class Clase3_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Bienvenido a Git Tema 3 Practica 2");
+
+		// Incluyo esta nueva linea
+		System.out.print("Mi nombre es Nombre y Apellidos");
+
 	}
 
 }
